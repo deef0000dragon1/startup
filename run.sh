637c1636cb7ad8b8ce239c086dcef3ffd1e38c26
Jeffrey Koehler <deef@deef.tech>
2019-03-19 21:07:28 -0500

    Add code to force ssh keys.

--- run.sh
+++ run.sh
@@ -10,18 +10,41 @@ fi
 #move bash aliases to home directory
 cp -f .bash_aliases ~/.bash_aliases
 
-source .
-#install ssh if it does not exist.
-sudo apt install openssh
-sudo apt install speedtest-cli
-sudo apt install htop
+#install programs
+sudo apt update
+sudo apt upgrade
+sudo apt install openssh htop
+
+
+#force no root login over ssh
+sudo sed -i 's/#\?\s*\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+#force no password authentication over ssh
+sudo sed -i 's/#\?\s*\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+#force allow public key authentication
+sudo sed -i 's/#\?\s*\(PubkeyAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
 
 #replace authorized keys file to .ssh/authorized_keys
 mkdir ~/.ssh
 touch ~/.ssh/authorized_keys
 cp -f authorized_keys ~/.ssh/authorized_keys
 
-##Set up git config etc.
+
+
+#set git user
+read -p "Enter name for git [Jeffrey Koehler Auto]: " name
+name=${name:-"Jeffrey Koehler Auto"}
+git config --global user.name $name
+
+#set git email
+read -p "Enter name for git [jeffreykoehlerauto@deef.tech]: " email
+email=${email:-"jeffreykoehlerauto@deef.tech"}
+git config --global user.email $email
 
 #set colors
 eval $(dircolors -b .dircolors)
+
+
+#reload the bash file, reloading all the 
+source ~/.bashrc
