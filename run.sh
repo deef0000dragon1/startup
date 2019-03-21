eecd30c63293fcd0f3adb1bd16076b88488723bf
deef <deef551@gmail.com>
2019-03-21 17:56:43 -0500

    remove run as root requirement. Sudo will auto ask for it.

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 #This program runs an automated script that loads in all the necessary files such as public keys and alias files and file structures
 
 #check for sudo
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root"
-	exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+#	echo "This script must be run as root"
+#	exit 1
+#fi
 
 #move bash aliases to home directory
 cp -f .bash_aliases ~/.bash_aliases
